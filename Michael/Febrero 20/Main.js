1e03b819467f3f6a1b34277a2a6eec4df1aed0db
Michael-Lozano <maicolozano8@gmail.com>
2023-02-20 10:58:00 -0500

    Subiendo Archivos al master

---
+++ Michael/Febrero 20/Main.js
@@ -0,0 +1,10 @@
+function mostrar() {
+    var Nombre = document.getElementById("Nombre").value
+    var Color = document.getElementById("Color").value
+    var Edad = document.getElementById("Edad").value
+    var Alimento = document.getElementById("Alimento").value
+
+
+
+    alert("Se guardadaron los datos Exitosamente " + "\n Nombre: " + Nombre + "\n Color: " + Color + "\n Edad: " + Edad + "\n Alimento: " + Alimento)
+}
